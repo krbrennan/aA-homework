a2518014fc7ebc899d21e5bc947d74b05bc85baa
Kevin Brennan <krb.brennan@gmail.com>
2018-02-23 08:10:46 -0800

    Commit w2d5 LRUCache hw

---
+++ w2d5_hw/hw.rb
@@ -0,0 +1,37 @@
+class LRUCache
+  attr_reader :cache, :size
+    def initialize(size)
+      @size = size
+      @cache = []
+    end
+
+    def count
+      # returns number of elements currently in cache
+      @cache.count
+    end
+
+    def add(el)
+      # adds element to cache according to LRU principle
+      if cache.include?(el)
+        cache.delete(el)
+        cache << el
+      elsif cache.count > size
+        cache.shift
+        cache << el
+      else
+        cache << el
+      end
+    end
+
+    def show
+      # shows the items in the cache, with the LRU item first
+      display
+    end
+
+    private
+    # helper methods go here!
+    def display
+      puts @cache
+    end
+
+  end
